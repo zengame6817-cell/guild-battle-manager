91aa999f148ee730a9569594dd6f043dea589c91
zengame6817-cell <zen.game.6817@gmail.com>
2026-07-28 17:29:51 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxjkdi_PWTcLgJKcQ1nZsaWsiyW3obOkmRUGKaCoVMRQ9w8vRXmrVcTrpE7rAX0L5mJ/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbyC3k5KeD0OqLp9soKwwjkRPBCUOZIfStwPU4QrFYF9FhryqnPs61zVVvTNfT4sXLWPWA/exec",
   REFRESH_INTERVAL_MS: 3000,
   DEFAULT_MODE: "normal"
 };
